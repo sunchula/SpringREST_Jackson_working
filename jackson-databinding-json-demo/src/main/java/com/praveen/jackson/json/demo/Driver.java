2086020f1ee38d8ede0300a06b79eacd049376dd
sunchula <sunchulapraveen@gmail.com>
2018-08-28 11:50:18 +0530

    Initial Commit

---
+++ jackson-databinding-json-demo/src/main/java/com/praveen/jackson/json/demo/Driver.java
@@ -0,0 +1,24 @@
+package com.praveen.jackson.json.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			
+			Student theStudent = mapper.readValue(new File("data/sample-lite.json"),Student.class);
+			
+			System.out.println("First Name:" +theStudent.getFirstName());
+			System.out.println("Last Name:" +theStudent.getLastName());
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
